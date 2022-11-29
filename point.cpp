667ade73547ad85cd18d83a07aec5b7d37faaa02
Vinny Allegra <vinnyallegra@gmail.com>
2022-11-29 10:24:59 -0500

    first commit

---
+++ point.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+#include "point.h"
+
+double Point::length(){
+    return sqrt (pow(x, 2) + pow(y, 2) + pow(z,2));
+}
+
+Point Point::operator- (Point& p){
+    Point * point = new Point(x - p.x, y - p.y, z - p.z);
+}
+
+void Point::print (void){
+    std::cout << "x: " << x << " y: " << y << " z: " << z << std::endl;
+}
---
+++ point.h
@@ -0,0 +1,13 @@
+class Point
+{
+    double x;
+    double y;
+    double z;
+    public:
+        Point () {};
+        Point (double x, double y, double z) { set (x, y, z); };
+        inline void set (double x, double y, double z){this->x = x; this->y = y; this->z = z;}
+        double length();
+        Point operator- (Point& p);
+        void print (void);
+};
